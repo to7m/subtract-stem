5d7478e8c3521cda9056d84ee31e3a75152032a1
to7m <ThomasHoweMain@gmail.com>
2025-02-10 03:29:53 +0000

    fixed multiple creation of grain buffer

--- tests_/grains.py
+++ tests_/grains.py
@@ -5,9 +5,10 @@ import subtract_stem_lib as ssl
 
 def _test_grains(
     *,
-    audio_len=10,
+    audio_len=10_000,
     start_i=-3000, interval_len=111, num_of_iterations=150,
     grain_len=999,
+    delay_audio_samples=0.0
 ):
     rng = np.random.default_rng(0)
 
@@ -18,16 +19,15 @@ def _test_grains(
         start_i=start_i,
         interval_len=interval_len,
         num_of_iterations=num_of_iterations,
-        grain_len=grain_len
+        grain_len=grain_len,
+        delay_audio_samples=delay_audio_samples
     )
-
-    grain = audio_to_hann_grains.out
 
     result_audio = audio.copy()
 
     add_grains_to_audio = ssl.AddGrainsToAudio(
-        grain,
-        start_i=start_i,
+        grain=audio_to_hann_grains.out,
+        start_i=audio_to_hann_grains.start_i,
         interval_len=interval_len,
         num_of_iterations=num_of_iterations,
         subtract=True,
