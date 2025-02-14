603f62779521dfcbde18f673475f0b20f96b66f0
to7m <ThomasHoweMain@gmail.com>
2025-02-14 05:03:20 +0000

    started eq profiles

---
+++ subtract_stem_lib/eq_profiles.py
@@ -0,0 +1,9 @@
+class SpectrumPairsToEqProfiles:
+    __slots__ = ["stem_spectrum", "mix_spectrum", "out"]
+
+    def __iter__(self):
+        def iterator():
+            for _ in lookbehind_range:
+                
+
+        return iterator()
