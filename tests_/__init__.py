030469ed60b28ebf4c241e61a2fb24b83fcdd29c
to7m <ThomasHoweMain@gmail.com>
2025-02-22 06:03:43 +0000

    include placeholder

--- tests_/__init__.py
+++ tests_/__init__.py
@@ -4,6 +4,7 @@ from .divide import safe_divide, ataabtrnfatbaa, all_divide
 from .io import test_io
 from .audio_grains import test_audio_grains
 #from .transforms import test_spectra, test_spectra_buffer, all_transforms
+#from .eq_profiles import
 
 
 def all_tests():
